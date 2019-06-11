5b49f0b3164926ba56f981625e873fe5a115caff
Yohann Leon <yohann@leon.re>
2019-06-11 02:19:43 +0200

    travels: can now stop and drag the globe

--- static/js/globe.js
+++ static/js/globe.js
@@ -1,21 +1,38 @@
 const width = 700;
 const height = 640;
-const config = {
-    speed: 0.005,
-    verticalTilt: 0,
-    horizontalTilt: 0
-}
+const speed = 0.005;
 const svg = d3.select('#globe').attr('width', width).attr('height', height);
 const projection = d3.geoOrthographic().translate([width / 2, height / 2]).scale(height / 2).clipAngle(90);
 const projectionOrbit = d3.geoOrthographic().translate([width / 2, height / 2]).scale(height / 2 + 30).clipAngle(90);
+const range = d3.scaleLinear().domain([0, width * 2]).range([-180, 180]);
 const path = d3.geoPath().projection(projection);
 const markerGroup = svg.append('g');
 const center = [width/2, height/2];
 
-let locations = [];
+var locations = [];
+var timer = null;
 
 drawGlobe();
 enableRotation();
+
+var drag = d3
+.drag()
+.subject(() => ({ x: range.invert(projection.rotate()[0]) }))
+.on('drag', function() {
+    pauseRotation();
+
+    projection.rotate([range(d3.event.x), 0]);
+    projectionOrbit.rotate([range(d3.event.x), 0]);
+
+    svg.selectAll('path').attr('d', path);
+
+    drawMarkers();
+})
+.on('end', function () { 
+  resumeRotation();
+});
+
+svg.call(drag);
 
 function drawGlobe() {
     d3.queue()
@@ -29,28 +46,50 @@ function drawGlobe() {
             .style('fill', '#b7efe9');
 
             locations = locationData;
+
             drawMarkers();
         });
 }
 
 function enableRotation() {
-    d3.timer(function (elapsed) {
-        projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
-        projectionOrbit.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
+    timer = d3.timer(function (elapsed) {
+        projection.rotate([speed * elapsed, 0]);
+        projectionOrbit.rotate([speed * elapsed, 0]);
 
-        svg.selectAll("path").attr("d", path);
+        svg.selectAll('path').attr('d', path);
+
+        drawMarkers();
+    });
+}
+
+function pauseRotation() {
+    timer.stop();
+}
+
+function resumeRotation() {
+    rot = projection.rotate()[0];
+    rotOrbit = projectionOrbit.rotate()[0];
+
+    timer.restart(function (elapsed) {
+        projection.rotate([rot + speed * elapsed, 0]);
+        projectionOrbit.rotate([rotOrbit + speed * elapsed, 0]);
+    
+        svg.selectAll('path').attr('d', path);
+
         drawMarkers();
     });
 }
 
 // Over function to be called on mouseover
 function over(d, i) {
-    d3.select(this).style("fill", "green");
+    pauseRotation();
+    d3.select(this).attr('r', 25).style('fill', 'white');
 }
 
 // Out function to be called on mouseout
 function out(d, i) {
-    d3.select(this).style("fill", null);
+    resumeRotation();
+    d3.select(this).attr('r', 8).style('fill', null);
 }
 
 function drawMarkers() {
